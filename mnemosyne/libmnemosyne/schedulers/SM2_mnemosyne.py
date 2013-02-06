7cf9c0c882f7f860c0801af31d828ef5a7a25d3d
Cole Robinson <crobinso@redhat.com>
2013-02-06 11:47:51 -0500

    SM2: Don't add noise to a interval that's <= 1 day
    Theory here is that if the next interval is less than a day away,
    the need to review is urgent enough that we shouldn't muck it up
    with any noise.

--- mnemosyne/libmnemosyne/schedulers/SM2_mnemosyne.py
+++ mnemosyne/libmnemosyne/schedulers/SM2_mnemosyne.py
@@ -187,7 +187,7 @@ class SM2Mnemosyne(Scheduler):
         return (0, 0, 1*DAY, 3*DAY, 4*DAY, 7*DAY) [grade]
 
     def calculate_interval_noise(self, interval):
-        if interval == 0:
+        if interval <= DAY:
             noise = 0
         elif interval <= 10 * DAY:
             noise = random.choice([0, DAY])
